0750d37d06cd425d618640c4b073185815f9ca7b
Fridaeriks <guserifrbc@student.gu.se>
2024-04-24 09:28:09 +0200

    upd
    upd

--- main_streamlit.py
+++ main_streamlit.py
@@ -6,14 +6,3 @@ df = pd.read_json('dataset.jsonl', lines=True)
 
 # Visa tabellen i Streamlit
 st.write(df)
-
-df = pd.read_json('subset')
-
-subset_df = df[['description', 'working_hours_type']]
-subset_df = subset_df.rename(columns={'description': 'Annons beskrivning', 'working_hours_type': 'Tid'})
-
-st.write("<span style='font-size: 20px;'>Här ser du de deltidsjobb som vi kan erbjuda:</span>", unsafe_allow_html=True)
-
-
-df = pd.read_json('dataset.jsonl')
-st.dataframe(subset_df)
