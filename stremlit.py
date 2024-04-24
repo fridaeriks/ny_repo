bead0fd7a76854f64d4521a03cc18fa2604704b4
Fridaeriks <guserifrbc@student.gu.se>
2024-04-24 09:26:16 +0200

    Update stremlit.py
    Lagt till rullistor etc.

--- stremlit.py
+++ stremlit.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 # Load the JSON file into a DataFrame 
 lines = []
-with open('dataset.jsonl', 'r') as file: 
+with open('dataset.jsonl', 'rb') as file: 
     for i, line in enumerate(file):
         lines.append(line.strip())
-        if i >= 99999:
+        if i >= 9999:
             break
 
 # Convert each line from JSON format to Python dictionary
@@ -36,10 +36,37 @@ subset = jobtech_dataset[[
     'keywords.extracted.occupation'
 ]]
 
-# Streamlit app
-st.title('Jobtech Dataset')
-# Display the DataFrame
+#Titel och text högst upp
+st.title('Vårt namn')
+st.markdown("Info om vårt projekt")
+st.markdown("---")
 st.write(subset)
+
+#Den gråa sidopanelen
+
+left_column = st.sidebar.empty()
+
+left_column.markdown("""
+<style>
+.left-column {
+    background-color: #f0f0f0;
+    width: 30%;
+    padding: 20px;
+    border-radius: 5px;
+}
+</style>
+""", unsafe_allow_html=True)
+
+left_column.markdown("""
+<div class="left-column" style="padding: 20px;">
+<h3>Filter</h3>
+<p>Här kan man lägga till text om man vill.</p>
+<hr>
+<p style="font-size: 18px;">Våra kontaktuppgifter:</p> 
+<p style="font-size: 12px;">Vera@devil.com</p> 
+                     
+</div>
+""", unsafe_allow_html=True)
 
 
 # Display description of a specific row
@@ -56,6 +83,48 @@ st.write('')
 
 # Extract unique values from the column "employer.workplace"
 places_list = subset['workplace_address.region'].unique().tolist()
+
+#Region rullista
+places_list = subset['workplace_address.region'].unique().tolist()
+selected_place = st.selectbox("Välj region:", places_list)
+filtered_subset = subset[subset['workplace_address.region'] == selected_place]
+st.write(filtered_subset)
+
+#Tidsomfattning rullista
+time_of_work = subset['working_hours_type.label'].unique().tolist()
+selected_time_of_work = st.selectbox("Välj tidsomfattning:", time_of_work)
+filtered_subset = subset[subset['working_hours_type.label'] == selected_time_of_work]
+st.write(filtered_subset)
+
+
+#Tabell där man kan filtrera med båda rullistorna
+
+column_aliases = {
+    'headline': 'Rubrik',
+    'number_of_vacancies': 'Antal Lediga Platser',
+    'description.text': 'Beskrivning',
+    'working_hours_type.label': 'Tidsomfattning',
+    'workplace_address.region': 'Region',
+    'workplace_address.municipality': 'Kommun'}
+
+places_list = subset['workplace_address.region'].unique().tolist()
+time_of_work = subset['working_hours_type.label'].unique().tolist()
+selected_place = st.selectbox("Select Region:", places_list)
+selected_time_of_work = st.selectbox("Select Time of Work:", time_of_work)
+
+filtered_subset = subset[(subset['workplace_address.region'] == selected_place) & 
+                         (subset['working_hours_type.label'] == selected_time_of_work)]
+
+filtered_subset = filtered_subset[['headline', 'number_of_vacancies', 'description.text', 
+                                   'working_hours_type.label', 'workplace_address.region', 
+                                   'workplace_address.municipality']]
+
+filtered_subset = filtered_subset.rename(columns=column_aliases) 
+st.write(filtered_subset)
+
+
+
+
 
 # Display the list of different places jobs are located
 st.write("Region:")
