2ec8595cbe29fe548f8073897c0075be3d1cfa2c
Fridaeriks <guserifrbc@student.gu.se>
2024-05-08 08:25:00 +0200

    Ny sida
    Ändringar på den nya sidan

--- welcome_strem.py
+++ welcome_strem.py
@@ -14,9 +14,32 @@ def run_main_app():
     subprocess.Popen(["streamlit", "run", "ny_strem.py"])
 
 
-st.write("")  # Lägger till lite extra utrymme
-col1, _, col2, _ = st.columns([1, 1, 6, 1])
-st.markdown ('         .   ')
+
+st.markdown(
+    """
+    <style>
+        .centered-button {
+            display: block;
+            margin: auto;
+            background-color: #0072c6; /* Blå färg */
+            color: white; /* Vit textfärg */
+            border: none; /* Ingen kantlinje */
+            padding: 10px 20px; /* Padding för knappens storlek */
+            border-radius: 5px; /* Runda kanter */
+            cursor: pointer; /* Visa pekare vid hover */
+            font-size: 16px; /* Storlek på text */
+        }
+        .centered-button:hover {
+            background-color: #005ea3; /* Ändra färg vid hover */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+col1, _, col2, _ = st.columns([1, 1, 1.5, 1])
 with col2:
-    if st.button("Start"):
+    if st.button("Kom igång"):
         run_main_app()
+
+
